6dc088314351821d34a8636b9dd2598ef1260c30
plrtp <plrtp@mail.ru>
2025-02-02 14:40:30 +0300

    deleted /dist

--- dist/scripts/script.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const canvas = document.querySelector('#background');
-const ctx = canvas.getContext('2d');
-const resizeCanvas = () => {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-};
-window.addEventListener('resize', resizeCanvas);
-resizeCanvas();
